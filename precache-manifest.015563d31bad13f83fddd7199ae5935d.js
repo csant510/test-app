1834583e43ee9fe21924794c9a221d096904e893
csant510 <james.sant83@gmail.com>
2020-03-10 22:18:42 -0700

    Updates

---
+++ precache-manifest.015563d31bad13f83fddd7199ae5935d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81679bf372f02b554f6a7050bee30f20",
+    "url": "/csant510/test-app/index.html"
+  },
+  {
+    "revision": "71cda7a519eddfbcd98b",
+    "url": "/csant510/test-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "437ee76fab01708f2c8e",
+    "url": "/csant510/test-app/static/js/2.f8969ca5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/csant510/test-app/static/js/2.f8969ca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71cda7a519eddfbcd98b",
+    "url": "/csant510/test-app/static/js/main.77d16c6e.chunk.js"
+  },
+  {
+    "revision": "3272522f94974c94d994",
+    "url": "/csant510/test-app/static/js/runtime-main.9cbcbe3c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/csant510/test-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
